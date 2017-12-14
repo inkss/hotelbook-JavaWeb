4be18a3103c9f1af68aea6194af21095338d6ec7
buting <szhiy1996@gmail.com>
2017-12-14 20:11:11 +0800

    清空库

--- src/webapp/js/global.js
+++
@@ -1,7 +0,0 @@
-//服务器端url
-var baseUrl = "http://localhost:8080/hb";
-
-//不受这个影响的地址有：
-//1. win10.js 854行 857行
-//2. romeTypeTable.js 88行
-//3. CommonFilter.java 54行
--- src/webapp/loginTable.jsp
+++
@@ -1,9 +0,0 @@
-<%@ page contentType="text/html;charset=UTF-8"  %>
-<html>
-<head>
-    <title>Title</title>
-</head>
-<body>
-
-</body>
-</html>
